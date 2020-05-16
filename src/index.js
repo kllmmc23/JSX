d64361bdd6c45167623de7f3ac2f1abf13d1e7c2
Michael <kllmmc23@gmail.com>
2020-05-16 11:38:17 -0600

    fav foods practice

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 ReactDOM.render(
-  <h1>hello 1</h1>,
+  <div>
+    <h1>My Favourite Foods</h1>
+    <ul>
+      <li>Bacon</li>
+      <li>Jamon</li>
+      <li>Noodles</li>
+    </ul>
+  </div>,
 
   document.getElementById("root")
 );
