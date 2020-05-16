84448b2d81780ded78f42acb62a8bca4ffe6f79a
Michael <kllmmc23@gmail.com>
2020-05-16 12:31:20 -0600

    jsx expressions practice

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const fname = 'Michael';
-const lname = "Elliott";
-const number = 100;
+const YOURNAME = "Michael";
+var date = new Date();
+const CURRENTYEAR = date.getFullYear();
 
 ReactDOM.render(
   <div>
-    <h1> {fname} {lname}'s Favorite foods.</h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Sweet Potatoes {number}%</li>
-      <li>Eggs with mushroom</li>
-    </ul>
-  </div>
-
-  ,document.getElementById("root")
+    <p>Created by {YOURNAME}</p>
+    <p>Copyright {CURRENTYEAR}</p>
+  </div>,
+  document.getElementById("root")
 );
