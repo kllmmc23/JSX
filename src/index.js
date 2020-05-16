cb86981b6b71dd063caa1fed762d4c0a08d8b062
Michael <kllmmc23@gmail.com>
2020-05-16 17:07:33 -0600

    import & export and modules on js ES6

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App.jsx'
-const rootElement =document.getElementById('root');
+import React from "react";
+import ReactDOM from "react-dom";
+import { add, multiply, subtract, divide } from "./calculator.js";
 
-ReactDOM.render(<App />,rootElement);
+const rootElement = document.getElementById("root");
+
+ReactDOM.render(
+  <ul>
+    <li>{add(1, 2)}</li>
+    <li>{multiply(2, 3)}</li>
+    <li>{subtract(7, 2)}</li>
+    <li>{divide(5, 2)}</li>
+  </ul>,
+  rootElement
+);
